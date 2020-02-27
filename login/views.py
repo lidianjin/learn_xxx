a9fba1937d3f5900d7e4bc63d2d47d2b9febb966
lidianjin <2284631918@qq.com>
2020-02-27 09:56:14 +0800

    架构初步视图

--- login/views.py
+++ login/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def index(request):
+    """主页"""
+    return render(request, 'login/index.html')
+
+
+def login(request):
+    """登录"""
+    return render(request, 'login/login.html')
+
+
+def register(request):
+    """注册"""
+    return render(request, 'login/register.html')
+
+
+def logout(request):
+    """登出"""
+    return redirect('/login/')
