9f2ed6645374c7e1c564d5cbae600f83a0f2bce8
lidianjin <2284631918@qq.com>
2020-02-27 14:45:43 +0800

    添加显示信息

--- login/views.py
+++ login/views.py
@@ -15,6 +15,7 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        message = '请检查填写的内容'
         # 用户名和密码都不为空
         if username.strip() and password:
             # 用户名字符合法性验证
@@ -23,10 +24,16 @@ def login(request):
             try:
                 user = models.User.objects.get(name=username)
             except:
-                return render(request, 'login/login.html')
+                message = '用户不存在'
+                return render(request, 'login/login.html', {'message': message})
 
             if user.password == password:
                 return redirect('/index/')
+            else:
+                message = '密码不正确'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request, 'login/login.html', {'message': message})
     return render(request, 'login/login.html')
 
 
