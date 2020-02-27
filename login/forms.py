90e8a068caa2b6a9f51f2481a4c83ddc43d44445
lidianjin <2284631918@qq.com>
2020-02-27 15:17:48 +0800

    手动渲染表单字段

--- login/forms.py
+++ login/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class UserForm(forms.Form):
     """用户表单"""
-    username = forms.CharField(label='用户名', max_length=128)
-    password = forms.CharField(label='密码', max_length=256, widget=forms.PasswordInput)
+    username = forms.CharField(label='用户名', max_length=128, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '用户名', 'autofocus': ''}))
+    password = forms.CharField(label='密码', max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '密码'}))
