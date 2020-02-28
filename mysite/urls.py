197f2a7d1950d57267681461b88b43980541b56b
lidianjin <2284631918@qq.com>
2020-02-28 10:06:05 +0800

    添加captcha二级路由url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 from login import views
 
@@ -24,4 +25,5 @@ urlpatterns = [
     path('login/', views.login),
     path('register/', views.register),
     path('logout/', views.logout),
+    path('captcha/', include('captcha.urls')),
 ]
